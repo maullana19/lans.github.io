e973f2c6584809cfdd677b67f0cd8cc0721babe1
maullana19 <dedemaulana162@gmail.com>
2022-07-05 00:17:17 +0700

    First

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <div
+      id="Apps"
+      className="App"
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {" "}
+      Hello
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home/Index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// Function Components
+
+const Kotak = () => {
+  return (
+    <div>
+      <h2>Ini Kotak 1</h2>
+    </div>
+  );
+};
+
+export default Kotak;
