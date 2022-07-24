13f891764c3b4ecb03b581e13f40a669ca361d97
maullana19 <dedemaulana162@gmail.com>
2022-07-24 23:34:23 +0700

    My Next Project

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="3xl font-bold text-cyan-700">Welcome to my website</h1>
+      <p>Here u can see my profile </p>
+    </div>
+  );
+}
